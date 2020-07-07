f118808c78fd8787ef54b0b8ed352215fb3c9204
hdias <diaslinoh@gmail.com>
2020-07-07 18:43:17 -0300

    Server ApkCombo

--- upload_file.php
+++ upload_file.php
@@ -1,13 +1,14 @@
 <?php
 $baseUrl = "https://m.apkpure.com/android/%s/download?from=details";
 $apkFile = null;
+
 try {
     // error_reporting(0);
     // ini_set('display_errors', 0);
 
 
     if (isset($_GET['package'])) {
-        $apkUrl = getApkUrl(sprintf($baseUrl, $_GET['package']));
+        $apkUrl = getApkUrlCombo($_GET['package']);
 
         if ($apkUrl) {
             $apkFile = __DIR__ . "/downloads/tmp_" . time();
@@ -214,6 +215,54 @@ function getApkUrl($url = "")
                 return null;
             }
         } else {
+            return null;
+        }
+    } catch (\Throwable $th) {
+        return null;
+    }
+}
+function getApkUrlCombo($package = "")
+{
+    try {
+        $host = "https://apkcombo.com";
+        $baseUrl = "https://apkcombo.com/pt-br/%s/download/apk";
+        $url = sprintf($baseUrl, $package);
+        if (preg_match("/href=\'(.+)\'/i", shell_exec("curl " . $url), $m)) {
+            $m = count($m) > 1 ? $m[1] : null;
+            if ($m) {
+                $m = $host . $m;
+                if (preg_match("/<a href=\"(.+)\" class=\"app\"/i", shell_exec("curl " . $m), $m)) {
+                    $m = count($m) > 1 ? $m[1] : null;
+                    if ($m) {
+                        $tmpHeaders = @get_headers($m);
+                        if ($tmpHeaders && strpos($tmpHeaders[0], "200")) {
+                            $file = [
+                                "url" => $m,
+                                "type" => null,
+                                "filename" => null
+                            ];
+                            // print_r($tmpHeaders);
+                            foreach ($tmpHeaders as $header) {
+                                if (preg_match("/Content-Type: (.*)/i", $header, $tmp)) {
+                                    $tmp = count($tmp) > 1 ? $tmp[1] : null;
+                                    $file['type'] = $tmp;
+                                }
+                                if (preg_match("/filename=\"(.*)\"/i", $header, $tmp)) {
+                                    $tmp = count($tmp) > 1 ? $tmp[1] : null;
+                                    $file['filename'] = $tmp;
+                                }
+                            }
+                            return $file;
+                        } else {
+                            return null;
+                        }
+                    } else {
+                        return null;
+                    }
+                } else {
+                    return null;
+                }
+            }
             return null;
         }
     } catch (\Throwable $th) {
